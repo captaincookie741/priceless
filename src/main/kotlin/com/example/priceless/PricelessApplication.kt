82857cf5b4df8a48196a356c2c4ce91a4b930124
captaincookie741 <pan.zhu@thoughtworks.com>
2024-06-19 16:12:53 +0800

    initialized a maven repo.

---
+++ src/main/kotlin/com/example/priceless/PricelessApplication.kt
@@ -0,0 +1,11 @@
+package com.example.priceless
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PricelessApplication
+
+fun main(args: Array<String>) {
+    runApplication<PricelessApplication>(*args)
+}
